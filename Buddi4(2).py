b0febf085c3610d73ea4d7c1541f898901353a1f
RahulsaravananS <132289975+RahulsaravananS@users.noreply.github.com>
2024-05-07 21:47:56 +0530

    Update Buddi4(2).py

--- Buddi4(2).py
+++ Buddi4(2).py
@@ -2,14 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 m = 0
 sde=3
+sdev=2
 sd = 1
 x = np.linspace(-5*sd, 5*sd)
 f = (1 / (sd * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - m) / sd) ** 2)
-
+fev=(1 / (sdev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - m) / sdev) ** 2)
 fe= (1 / (sde * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - m) / sde) ** 2)
 
-plt.plot(x, f, color='blue', label='Bell Curve')
-plt.plot(x, fe, color='red', label='Bell Curve')
+plt.plot(x, f, color='blue', label='SD 1 Bell Curve')
+plt.plot(x, fev, color='black', label='SD 2 Bell Curve')
+plt.plot(x, fe, color='red', label='SD 3 Bell Curve')
 
 plt.plot()
 plt.title("Normal Distribution ")
