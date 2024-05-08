921391489a19d1c04dccfcdb76cafa8b1cc9b88e
RahulsaravananS <132289975+RahulsaravananS@users.noreply.github.com>
2024-05-08 10:34:50 +0530

    Update Buddi4(2).py

--- Buddi4(2).py
+++ Buddi4(2).py
@@ -4,14 +4,17 @@ m = 0
 sde=3
 sdev=2
 sd = 1
-x = np.linspace(-5*sd, 5*sd)
-f = (1 / (sd * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - m) / sd) ** 2)
-fev=(1 / (sdev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - m) / sdev) ** 2)
-fe= (1 / (sde * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - m) / sde) ** 2)
+x1 = np.linspace(m-(5*sd), m+(5*sd))
+x2 = np.linspace(m-(5*sdev), m+(5*sdev))
+x3 = np.linspace(m-(5*sde), m+(5*sde))
 
-plt.plot(x, f, color='blue', label='SD 1 Bell Curve')
-plt.plot(x, fev, color='black', label='SD 2 Bell Curve')
-plt.plot(x, fe, color='red', label='SD 3 Bell Curve')
+f = (1 / (sd * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x1 - m) / sd) ** 2)
+fev=(1 / (sdev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x2 - m) / sdev) ** 2)
+fe= (1 / (sde * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x3 - m) / sde) ** 2)
+
+plt.plot(x1, f, color='blue', label='SD 1 Bell Curve')
+plt.plot(x2, fev, color='black', label='SD 2 Bell Curve')
+plt.plot(x3, fe, color='red', label='SD 3 Bell Curve')
 
 plt.plot()
 plt.title("Normal Distribution ")
