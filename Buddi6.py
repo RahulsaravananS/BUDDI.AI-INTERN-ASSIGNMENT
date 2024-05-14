d99870271505cf8929f62bf242f2806a958eba05
RahulsaravananS <132289975+RahulsaravananS@users.noreply.github.com>
2024-05-14 10:20:57 +0530

    Update Buddi6.py

--- Buddi6.py
+++ Buddi6.py
@@ -33,17 +33,20 @@ epoch_list = [0]  # List to store epochs
 error_list = [error_init]  # List to store errors
 
 converged = False  # Flag to check convergence
-
+B0=[]
+B1=[]
 # Gradient Descent loop
 while not converged:
     y_pred = b0 + b1*x  # Predicted y values using current parameters
     grad_b0 = -2*np.mean((y - y_pred))  # Gradient of intercept
     grad_b1 = -2*np.mean((y - y_pred)*x)  # Gradient of slope
-    
     # Updating parameters using gradients and learning rate
     b0 -= lr * grad_b0
     b1 -= lr * grad_b1
-    
+    # store the B0 and B1 values in list
+    B0.append(b0)
+    B1.append(b1)
+
     new_error = np.mean((y - (b0 + b1*x))**2)  # New error calculation
     epoch += 1  # Incrementing epoch
     
@@ -62,10 +65,23 @@ print("Closed Form: Beta0:", b0_closed, "Beta1:", b1_closed, "Error value:", err
 print("Gradient Descent: Beta0:", b0, "Beta1:", b1, "Error value:", error, "epoch:", epoch)
 
 # Plotting error convergence
+
 plt.plot(epoch_list, error_list,label="Error",color='blue')
 plt.xlabel('Epoch')
 plt.ylabel('Mean Squared Error')
 plt.title('Gradient Descent: Error Convergence')
 plt.figtext(0.5, 0.01, "The above graph represent the error convergence of linear regression model using gradient descent", ha="center", fontsize=10, bbox={"facecolor": "brown", "alpha": 0.5, "pad": 5})
 plt.legend()
+
+#plot the 3D graph
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_trisurf(B0,B1,epoch_list, cmap='viridis',label="Change of B0 and B1 with epoch",edgecolor='none')
+plt.figtext(0.5, 0.01, "The above 3D graph represent the change of B0 and B1 with respect to epochs ", ha="center", fontsize=10, bbox={"facecolor": "brown", "alpha": 0.5, "pad": 5})
+plt.legend()
+ax.set_xlabel('Beta0(B0)')
+ax.set_ylabel('Beta1(B1')
+ax.set_zlabel('Epoch')
+ax.set_title('Surface Plot')
 plt.show()
